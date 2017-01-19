1a618ee5b98173cb0c8c48063d09999dce94803e
K9042 <K9042@student.jamk.fi>
2017-01-19 14:09:07 +0200

    Done

--- teh1/Program.cs
+++ teh1/Program.cs
@@ -10,6 +10,15 @@ namespace teh1
     {
         static void Main(string[] args)
         {
+            int luku;
+
+            Console.Write("Anna luku 1, 2 tai 3. ");
+            luku = int.Parse(Console.ReadLine());
+
+            if (luku == 1) Console.WriteLine("Annoit luvun yksi.");
+            else if (luku == 2) Console.WriteLine("Annoit luvun kaksi.");
+            else if (luku == 3) Console.WriteLine("Annoit luvun kolme.");
+            else Console.WriteLine("Joku muu luku.");
         }
     }
 }
